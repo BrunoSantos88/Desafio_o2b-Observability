b5d4ce0c75bce72e933ded336b9ce74937337fd7
BrunoSantos88 <brunosantos88>
2023-11-08 12:39:03 -0300

    prometheus

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-sudo add-apt-repository ppa:longsleep/golang-backports -y
-sudo apt update -y
-sudo apt install golang-1.18 -y
-sudo export PATH=$PATH:/usr/local/go/bin
+add-apt-repository ppa:longsleep/golang-backports -y
+apt update -y
+apt install golang-1.18 -y
+export PATH=$PATH:/usr/local/go/bin
