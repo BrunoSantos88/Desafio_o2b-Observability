0f8925bb7fe9810b8e99fa2900a7cef818c36cc7
Bruno Santos F. Da Silva <91704169+BrunoSantos88@users.noreply.github.com>
2023-11-09 14:25:33 -0300

    Update server.go

--- server.go
+++ server.go
@@ -3,7 +3,15 @@ package main
 import (
    "fmt"
    "net/http"
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+	"strings"
 
+	"github.com/gree-gorey/geoip-exporter/pkg/conn"
    "github.com/prometheus/client_golang/prometheus"
    "github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -13,6 +21,17 @@ var pingCounter = prometheus.NewCounter(
        Name: "ping_request_count",
        Help: "No of request handled by Ping handler",
    },
+   )
+)
+
+  var (
+	location = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "job_location",
+			Help: "Location connections number",
+		},
+		[]string{"location"},
+	)
 )
 
 func ping(w http.ResponseWriter, req *http.Request) {
@@ -26,4 +45,44 @@ func main() {
    http.HandleFunc("/ping", ping)
    http.Handle("/metrics", promhttp.Handler())
    http.ListenAndServe(":8090", nil)
+}
+
+func main() {
+	addr := flag.String("web.listen-address", ":9300", "Address on which to expose metrics")
+	interval := flag.Int("interval", 180, "Interval fo metrics collection in seconds")
+	blackList := flag.String("blacklist", "", "Addresses blacklist to filter out from results")
+	debug := flag.Bool("debug", false, "Debug log level")
+	flag.Parse()
+	prometheus.MustRegister(location)
+	http.Handle("/metrics", prometheus.Handler())
+	go run(int(*interval), *debug, *blackList)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+func run(interval int, debug bool, blackList string) {
+	for {
+		var wg sync.WaitGroup
+		c := conn.Connections{}
+		wg.Add(1)
+		blackListArr := strings.Split(blackList, ",")
+		b := make(map[string]bool)
+		for _, key := range blackListArr {
+			b[key] = true
+		}
+		p := conn.Params{UseWg: true, Wg: &wg, BlackList: b}
+		go c.RunJob(&p)
+		wg.Wait()
+		if debug == true {
+			ser, err := json.Marshal(c)
+			if err != nil {
+				log.Println(err)
+			}
+			log.Println(string(ser))
+		}
+		location.Reset()
+		for code, number := range c.ConnectionsByCode {
+			location.With(prometheus.Labels{"location": code}).Set(float64(number))
+		}
+		time.Sleep(time.Duration(interval) * time.Second)
+	}
 }
