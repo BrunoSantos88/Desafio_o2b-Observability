533d25eab02afbafc889d6e42e6c4a64d0366291
BrunoSantos88 <brunosantos88>
2023-11-11 03:27:48 -0300

    dockerizando

--- app/server.go
+++ app/server.go
@@ -3,7 +3,11 @@ package main
 import (
    "fmt"
    "net/http"
+   "fmt"
+   "log"
+   "net"
 
+   "github.com/oschwald/geoip2-golang"
    "github.com/prometheus/client_golang/prometheus"
    "github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -20,10 +24,34 @@ func ping(w http.ResponseWriter, req *http.Request) {
    fmt.Fprintf(w, "pong")
 }
 
+func loadGeoIPDatabase() (*geoip2.Reader, error) {
+   db, err := geoip2.Open("path/to/GeoIP2-City.mmdb")
+   if err != nil {
+       return nil, err
+   }
+   return db, nil
+}
+
 func main() {
    prometheus.MustRegister(pingCounter)
 
    http.HandleFunc("/ping", ping)
    http.Handle("/metrics", promhttp.Handler())
    http.ListenAndServe(":8090", nil)
+}
+
+func getGeoIPInfo(db *geoip2.Reader, ipAddress string) {
+   ip := net.ParseIP(ipAddress)
+   record, err := db.City(ip)
+   if err != nil {
+       log.Fatal(err)
+   }
+   fmt.Printf("City: %s\n", record.City.Names["en"])
+   fmt.Printf("Country: %s\n", record.Country.Names["en"])
+   fmt.Printf("Latitude: %f\n", record.Location.Latitude)
+   fmt.Printf("Longitude: %f\n", record.Location.Longitude)
+}
+
+func closeGeoIPDatabase(db *geoip2.Reader) {
+   db.Close()
 }
