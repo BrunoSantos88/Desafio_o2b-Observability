0f6926a8cd204869a02f7208e2ceff2b48985231
BrunoSantos88 <brunosantos88>
2023-11-13 16:06:03 -0300

    jaeger/

--- app/server.go
+++ app/server.go
@@ -3,14 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/opentracing/opentracing-go"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/uber/jaeger-client-go"
-	"github.com/uber/jaeger-client-go/config"
-	"github.com/uber/jaeger-lib/metrics"
 )
 
 var pingCounter = prometheus.NewCounter(
@@ -22,42 +17,13 @@ var pingCounter = prometheus.NewCounter(
 
 func ping(w http.ResponseWriter, req *http.Request) {
 	pingCounter.Inc()
-
-	// Adiciona rastreamento ao manipulador "ping"
-	span, ctx := opentracing.StartSpanFromContext(req.Context(), "ping-handler")
-	defer span.Finish()
-
 	fmt.Fprintf(w, "pong")
 }
 
 func main() {
 	prometheus.MustRegister(pingCounter)
 
-	// Configuração do Jaeger
-	cfg := &config.Configuration{
-		ServiceName: "meu-app-go",
-		Sampler: &config.SamplerConfig{
-			Type:  "const",
-			Param: 1,
-		},
-		Reporter: &config.ReporterConfig{
-			LogSpans: true,
-		},
-	}
-
-	tracer, closer, err := cfg.NewTracer(
-		config.Logger(jaeger.StdLogger),
-		config.Metrics(metrics.NullFactory),
-	)
-	if err != nil {
-		fmt.Printf("Erro ao criar o tracer: %v\n", err)
-		os.Exit(1)
-	}
-	defer closer.Close()
-
-	opentracing.SetGlobalTracer(tracer)
-
 	http.HandleFunc("/ping", ping)
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe("0.0.0.0:8090", nil)
+	http.ListenAndServe(":8090", nil)
 }
